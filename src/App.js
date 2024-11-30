c95720b6ee0affc83332156873159cc3c333befb
Nitin Bisht <3498georgian@gmail.com>
2024-11-30 14:05:14 +0530

    Install Tailwind and also corrected the Readme

--- src/App.js
+++ src/App.js
@@ -2,18 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <h1 className="text-3xl font-bold underline flex justify-center items-center h-screen">
+      Hello world!
+    </h1>
+    </>
   );
 }
 
